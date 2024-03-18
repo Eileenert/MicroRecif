2ea6d7c4bcddb24adb30953e2f80ab076611d716
Orjowane <orjowanekaki@gmail.com>
2024-03-18 17:31:27 +0100

    j'ai commit après avoir déplacé dans shape

--- shape.cc
+++ shape.cc
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -22,12 +23,76 @@ S2d Segments::get_extr(){
     return extr;
 }
 
-void onSegment(){
+void ecart_angulaire(double& ecart, double angle1, double angle2){ //section 2.1, angle1 c'est alphak et angle2 c'est alphak+1
+    double ecart(0.);
+    if (angle2 >= 0){
+        ecart = M_PI - (angle2-angle1);
+    }
 
-}// détection d'appartenance d'un point q à un segment pr (q,p,r sont des points) section 2.2.2
+    if (angle2 < 0){
+        ecart = M_PI - (angle1+angle2); 
+    }
+}
 
-void doIntersection(){
+bool bool_superpo(double ecart){
+    double ecart(0.), a1(0.), a2(0.);
+    bool superposition(0);
 
-}//donne le résultat final sur la présence d'une intersection ou d'un alignement 
+    ecart_angulaire(ecart, a1, a2);
+
+    if (ecart == 0){
+        superposition = 1;
+    }
+
+    return superposition;
+}//section 2.1//shape à définir
+
+//bool bool_intersect_superpo(){
+//}//à finir ?? je sais pas si on doit faire ça aussi
+
+bool onSegment(S2d p, S2d q, S2d r){
+     //if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) && 
+      //  q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y)) 
+    double s((r.x-p.x)*(q.x-p.x)+(r.y-p.y)*(q.y-p.y));
+    double c(pow((r.x-p.x),2)+pow((r.y-p.y),2)); //c c'est le (X^2 + Y^2) du vecteur pr
+    double pr(pow(c,1/2));// pr c'est la norme du vecteur pr
+    double x(s/pr); //x c'est le truc que le prof veut qu'on calcul dans la section 2.2.2 (tkt on a pas besoin de comprendre exactement le pourquoi du calcul, c'est donné)
+
+    if ((-epsil_zero <= x) && (x <= (pr+epsil_zero)))
+        return true; 
+  
+    return false; 
+}
+
+int orientation(S2d p, S2d q, S2d r) 
+{ 
+    int val = (q.y - p.y) * (r.x - q.x) - 
+              (q.x - p.x) * (r.y - q.y); 
+  
+    if (val == 0) return 0;
+  
+    return (val > 0)? 1: 2; 
+}
+
+bool doIntersect(S2d p1, S2d q1, S2d p2, S2d q2) 
+{ 
+    int o1 = orientation(p1, q1, p2); 
+    int o2 = orientation(p1, q1, q2); 
+    int o3 = orientation(p2, q2, p1); 
+    int o4 = orientation(p2, q2, q1); 
+  
+    if (o1 != o2 && o3 != o4) 
+        return true; 
+  
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
+   
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
+   
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
+  
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
+  
+    return false; 
+} 
 
 // TESTS ==============================
