718e52abbcfddde1f625641d103710d87fcfc010
Orjowane Ghiath M Kaki <okaki@ic-24MA-spr-019.intranet.epfl.ch>
2024-04-12 16:09:36 +0200

    chsnges

--- gui.h
+++ gui.h
@@ -8,13 +8,37 @@
 
 class Gui : public Gtk::Window{
     public:
+        // Constructeur de Gui
+        Gui() {
+        // Création des boutons
+        m_Button_exit.set_label("Exit");
+        m_Button_open.set_label("Open");   
+        m_Button_save.set_label("Save"); 
+        m_Button_start.set_label("Start");
+        m_Button_step.set_label("Step");  
+
+        // Ajout des boutons à la boîte verticale
+        m_Box.pack_start(m_Button_exit, Gtk::PACK_EXPAND_WIDGET);
+        m_Box.pack_start(m_Button_open, Gtk::PACK_EXPAND_WIDGET);
+        m_Box.pack_start(m_Button_save, Gtk::PACK_EXPAND_WIDGET); 
+        m_Box.pack_start(m_Button_start, Gtk::PACK_EXPAND_WIDGET);
+        m_Box.pack_start(m_Button_step, Gtk::PACK_EXPAND_WIDGET); 
+
+        // Ajout de la boîte à la fenêtre
+        add(m_Box);
+
+        // Affichage des éléments
+        show_all();
+        }
 
     protected:
-        //Gtk::Button m_Button_start;
-        //Gtk::Button m_Button_step;
+        Gtk::Button m_Button_exit;
+        Gtk::Button m_Button_open;
+        Gtk::Button m_Button_save;
+        Gtk::Button m_Button_start;
+        Gtk::Button m_Button_step;
+        Gtk::Box m_Box{Gtk::ORIENTATION_VERTICAL}; 
 
 };
-
-
 
 #endif 
--- projet.cc
+++ projet.cc
@@ -12,5 +12,12 @@ int main(int argc, char * argv[]){
     start_simulation(argv[1]);
 
     auto app = Gtk::Application::create();
+
+    // Taille de la fenêtre à l'initialisation
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    //return app->run(window); //c'est sensé nous permettre de déplacer la fenêtre ou changer sa taille
+
     return app->make_window_and_run<Gui>(1, argv);
 }
