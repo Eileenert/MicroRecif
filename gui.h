4173b0f969b74266a24a0354df0dc91888b9b9f7
Kakinette <orjowane.kaki@gmail.com>
2024-04-21 18:07:23 +0200

    Warnings y'a des warnings à cause de l'ordre dans le constructeur Gui je crois, le timer marche en tout cas

--- gui.h
+++ gui.h
@@ -8,6 +8,8 @@
 #include <gtkmm/drawingarea.h>
 #include "simulation.h"
 #include <string>
+#include <map> //pour timer
+
 
 
 struct Frame // Model Framing and window parameters
@@ -21,7 +23,8 @@ struct Frame // Model Framing and window parameters
 	int width;   // window width
 };
 
-class MyArea : public Gtk::DrawingArea{
+class MyArea : public Gtk::DrawingArea
+{
 public:
     MyArea();
     virtual ~MyArea();
@@ -37,24 +40,36 @@ private:
     Frame frame;
 };
 
-class Gui : public Gtk::Window{
-
+class Gui : public Gtk::Window
+{
 public:
-    // Constructeur de Gui
     Gui(char * nom_fichier);
     void change_button_name();
+    bool step_fonctionne();
+    void timer_start_stop(); //pour timer
     
-
 protected:
     bool name;
     std::string open_or_save;
     Simulation s ;
 
-    void on_button_clicked_exit();
+    void on_button_clicked_exit();//pour timer
     void on_button_clicked_open();
     void on_button_clicked_save();
-    void on_button_clicked_start();
+    void on_button_clicked_start();//pour timer
     void on_button_clicked_step();
+
+//début timer
+    // This is the standard prototype of the Timer callback function
+	bool on_timeout();
+
+    // to handle a single timer
+	bool timer_added; 
+	// to store a single timer disconnect request
+	bool disconnect; 
+	
+	const int timeout_value;
+//fin timer
 
     void on_file_dialog_response(int response_id, Gtk::FileChooserDialog* dialog);
 
@@ -65,37 +80,20 @@ protected:
     MyArea m_Area;
     
     //VERIFIER CONVENTION POUR LES NOMS
+    //prof a fait comme ça dans une serie, doute pour l'incremention
 
-    Gtk::Box m_Main_Box;
-    Gtk::Box m_General_Box;
-    Gtk::Box m_Buttons_Box;
-    Gtk::Box m_Naissance_Algue_Box;
-    Gtk::Box m_Info_Box;
-    Gtk::Box m_Maj_Box;
-    Gtk::Box m_Algue_Box;
-    Gtk::Box m_Corail_Box;
+    Gtk::Box m_Main_Box, m_General_Box, m_Buttons_Box, m_Naissance_Algue_Box;
+    Gtk::Box m_Info_Box, m_Info_txt_Box, m_Maj_Box, m_Algue_Box, m_Corail_Box;
     Gtk::Box m_Scavenger_Box;
 
-
-    Gtk::Label general_Label;
-    
-    Gtk::Button m_Button_Exit;
-    Gtk::Button m_Button_Open;
-    Gtk::Button m_Button_Save;
-    Gtk::Button m_Button_Start;
+    Gtk::Button m_Button_Exit, m_Button_Open, m_Button_Save, m_Button_Start;
     Gtk::Button m_Button_Step;
+    
     Gtk::CheckButton m_Naissance_Algue_CheckButton;
 
-    Gtk::Label m_Naissance_Algue_Label;
-
-    Gtk::Label info_Label;
-    Gtk::Label maj_Label;
-    Gtk::Label maj_Data_Label;
-    Gtk::Label nbr_Algue_Label;
-    Gtk::Label nbr_Algue_Data_Label;
-    Gtk::Label nbr_Corail_Label;
-    Gtk::Label nbr_Corail_Data_Label;
-    Gtk::Label nbr_Scavenger_Label;
+    Gtk::Label general_Label, m_Naissance_Algue_Label, info_Label, maj_Label;  
+    Gtk::Label maj_Data_Label, nbr_Algue_Label, nbr_Algue_Data_Label; 
+    Gtk::Label nbr_Corail_Label, nbr_Corail_Data_Label, nbr_Scavenger_Label;
     Gtk::Label nbr_Scavenger_Data_Label;
 };
 
